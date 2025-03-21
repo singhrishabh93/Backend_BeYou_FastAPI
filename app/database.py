9219223713ad3c20d25fc2d2bca59d542d220153
Rishabh Singh <singhrishabh1670@gmail.com>
2025-03-21 12:29:18 +0530

    Changes in databse.py and requirements.txt

--- app/database.py
+++ app/database.py
@@ -2,8 +2,10 @@ import certifi
 from motor.motor_asyncio import AsyncIOMotorClient
 from .config import MONGODB_URL, DB_NAME
 
+# For Railway deployment
 client = AsyncIOMotorClient(
     MONGODB_URL,
-    tlsCAFile=certifi.where()
+    tlsCAFile=certifi.where(),
+    serverSelectionTimeoutMS=5000
 )
 db = client[DB_NAME]
